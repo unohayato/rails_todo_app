c1f894f0d48a09df09dda1b6be409e28093c132b
unohayato <mamidori1p1p@gmail.com>
2024-05-03 19:43:15 +0900

    :fire:

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+  get "/signup",  to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  post "/logout", to: "sessions#destroy"
   resources :tasks
   resources :users
 end
