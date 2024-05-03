9196d03b10dd8ce1eb1f397839dd5dcf7b789f3d
unohayato <mamidori1p1p@gmail.com>
2024-05-03 21:16:22 +0900

    :fire:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence :true, length: {minimum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence :true, length: {minimum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
 end
