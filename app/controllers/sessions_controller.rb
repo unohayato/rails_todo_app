93458aecab54c0d5c7a894a437053b98b0301916
unohayato <mamidori1p1p@gmail.com>
2024-05-04 17:11:56 +0900

    :fire:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  # login
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  # logout
+  def destroy
+  end
 end
